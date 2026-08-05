35492b7355271b0d61ca2fea2e0e3d6e3ed5ac17
Nagendra943 <nagendra316341@gmail.com>
2026-08-05 23:11:52 +0530

    Modify DATABASE_URL for database connection
    Updated DATABASE_URL to remove SSL mode requirement.

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 #sessionmaker creates db session #base class for all database models
 
 #DATABASE_URL = "mysql+pymysql://root:nagendra%4012@localhost:3306/library_db"
-DATABASE_URL = "mysql://avnadmin:AVNS_KQO1d1hzqTLMEUo3Y9V@nagendra18-nagendra316341-fastapi.g.aivencloud.com:19846/defaultdb?ssl-mode=REQUIRED"
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_KQO1d1hzqTLMEUo3Y9V@nagendra18-nagendra316341-fastapi.g.aivencloud.com:19846/defaultdb"
 
 engine = create_engine(DATABASE_URL)
 #Engine is responsible for cerating a connection between fastapi and mysql db
